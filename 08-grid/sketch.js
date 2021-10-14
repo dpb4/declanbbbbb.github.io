a87b91805d7d1f58c758ab0cdad84eff78916096
declanbbbbb <declan.bnbg@gmail.com>
2021-10-14 12:00:47 -0600

    Update sketch.js

--- 08-grid/sketch.js
+++ 08-grid/sketch.js
@@ -1,11 +1,13 @@
 // Grid Demo
 
-let gridSize = 30;
+let gridSize = 60;
 let grid;
+let nesw = [[0, 1], [1, 0], [0, -1], [-1, 0], [0, 0]];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   grid = createRandom2DArray(gridSize, gridSize);
+  stroke(128);
 }
 
 function draw() {
@@ -15,19 +17,34 @@ function draw() {
 
 function mousePressed() {
   let cellWidth = width/gridSize;
-  let cellHeight = height/gridSize;
+  let cellHeight = width/gridSize;
 
   let cellX = Math.floor(mouseX/cellWidth);
-  let cellY = Math.floor(mouseY/cellHeight);
+  let cellY = Math.floor(mouseY/cellWidth);
 
-  if (grid[cellY][cellX] === 1) {
-    grid[cellY][cellX] = 0;
-  }
-  else if (grid[cellY][cellX] === 0) {
-    grid[cellY][cellX] = 1;
+  for (let move of nesw) {
+    let nCellX = cellX + move[0];
+    let nCellY = cellY + move[1];
+    if (nCellX < gridSize && nCellX >= 0 && nCellY < gridSize && nCellY >= 0) {
+      if (grid[nCellY][nCellX] === 1) {
+        grid[nCellY][nCellX] = 0;
+      }
+      else if (grid[nCellY][nCellX] === 0) {
+        grid[nCellY][nCellX] = 1;
+      }
+    }
   }
 }
+function keyTyped() {
+  if (key === 'b') {
+    grid = createEmpty2DArray(gridSize, gridSize, 1);
+  } else if (key === 'w') {
+    grid = createEmpty2DArray(gridSize, gridSize, 0);
 
+  }
+  // uncomment to prevent any default behavior
+  // return false;
+}
 function keyPressed() {
 
   grid = createEmpty2DArray(gridSize, gridSize);
@@ -36,7 +53,7 @@ function keyPressed() {
 
 function displayGrid() {
   let cellWidth = width/gridSize;
-  let cellHeight = height/gridSize;
+  let cellHeight = width/gridSize;
 
   for (let y=0; y<gridSize; y++) {
     for (let x=0; x<gridSize; x++) {
@@ -51,12 +68,12 @@ function displayGrid() {
   }
 }
 
-function createEmpty2DArray(rows, cols) {
+function createEmpty2DArray(rows, cols, val) {
   let grid = [];
   for (let y=0; y<rows; y++) {
     grid.push([]);
     for (let x=0; x<cols; x++) {
-      grid[y].push(0);
+      grid[y].push(val);
     }
   }
   return grid;
