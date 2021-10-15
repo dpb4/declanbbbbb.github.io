93262e4e4e45e5f9a6730deffe5363d29918a933
declanbbbbb <declan.bnbg@gmail.com>
2021-10-15 12:10:56 -0600

    game of life

--- gameoflife/.eslintrc.js
+++ gameoflife/.eslintrc.js
@@ -12,13 +12,13 @@ module.exports = {
     "rules": {
         // enable additional rules
         "indent": ["warn", 2],
-        "quotes": ["warn", "double"],
+
         "semi": ["error", "always"],
         "no-extra-parens": ["warn", "all"],
         "curly": ["error", "all"],
         "eqeqeq": ["warn", "always"],
         "no-floating-decimal": ["warn"],
-        "brace-style": ["warn", "stroustrup"],
+        "brace-style": ["warn", "1tbs"],
         "no-var": ["warn"],
 
         // override default options for rules from base configurations
@@ -27,7 +27,8 @@ module.exports = {
 
         // disable rules from base configurations
         "no-console": "off",
-        "no-unused-vars": "off"
+        "no-unused-vars": "off",
+        "no-undef": "off"
     },
     "globals": {
       //allow direct access to the DOM without errors
@@ -41,6 +42,7 @@ module.exports = {
 
       //p5js globals
       "storeItem": true,
+      "p5": true,
       "getItem": true,
       "clearStorage": true,
       "removeItem": true,
--- gameoflife/sketch.js
+++ gameoflife/sketch.js
@@ -10,12 +10,15 @@ let numSqH;
 let sqWid;
 
 let grid;
+let neighbors = [[1, 1], [0, 1], [-1, 1], [-1, 0], [-1, -1], [0, -1], [1, -1], [1, 0]];
+let speed;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
   sqWid = width/numSqW;
-  numSqH = floor(height/sqWid);
+  numSqH = floor(height/sqWid) + 1;
+  speed = 5;
 
   grid = Array(numSqW).fill(0).map(x => Array(numSqH).fill(0));
 }
@@ -23,7 +26,54 @@ function setup() {
 function draw() {
   background(255);
   displayGrid();
-  
+  // frameRate(60/speed);
+  if (keyIsDown(32) && frameCount % speed === 0) {
+    tickGrid();
+  }
+}
+
+function tickGrid() {
+  let nextGrid = Array(numSqW).fill(0).map(x => Array(numSqH).fill(0));
+
+  for (let x = 0; x < numSqW; x++) {
+    for (let y = 0; y < numSqH; y++) {
+      let cell = grid[x][y];
+      let num = numNeighbors(x, y);
+      if (cell === 1) {
+        if (num === 2 || num === 3) {
+          nextGrid[x][y] = 1;
+        }
+      } else {
+        if (num === 3) {
+          nextGrid[x][y] = 1;
+        }
+      }
+    }
+  }
+
+  grid = [...nextGrid];
+}
+
+function numNeighbors(x, y) {
+  let sum = 0;
+  for (let n of neighbors) {
+    let nx = x + n[0];
+    let ny = y + n[1];
+    if (nx >= 0 && nx < numSqW && ny >= 0 && ny < numSqH) {
+      if (grid[nx][ny] === 1) {
+        sum++;
+      }
+    }
+  }
+  return sum;
+}
+
+function fillRandom() {
+  for (let x = 0; x < numSqW; x++) {
+    for (let y = 0; y < numSqH; y++) {
+      grid[x][y] = round(random(0, 1));
+    }
+  }
 }
 
 function displayGrid() {
@@ -31,11 +81,18 @@ function displayGrid() {
     for (let y = 0; y < numSqH; y++) {
       if (grid[x][y] === 1) {
         fill(0);
-      } else {
-        fill(255);
+        rect(x*sqWid, y*sqWid, sqWid, sqWid);
       }
-      rect(x*sqWid, y*sqWid, sqWid, sqWid);
     }
+  }
+
+  //vertical
+  for (let i = 0; i < numSqW; i++) {
+    line(i*sqWid, 0, i*sqWid, height);
+  }
+  //horizontal
+  for (let i = 0; i < numSqH; i++) {
+    line(0, i*sqWid, width, i*sqWid);
   }
 }
 
@@ -51,7 +108,17 @@ function mouseClicked() {
 }
 
 function keyPressed() {
-  if (keyCode === SPACE) {
-    background(255, 0, 0);
+  // if (keyIsDown(32)) {
+  //   tickGrid();
+  // }
+  if (keyIsDown(88)) {
+    grid = Array(numSqW).fill(0).map(x => Array(numSqH).fill(0));
   }
+  if (keyIsDown(82)) {
+    fillRandom();
+  }
+}
+
+function mouseWheel(event) {
+  speed += event.delta/100;
 }
