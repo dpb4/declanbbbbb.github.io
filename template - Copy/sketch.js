fdb0005d78c37dd7e5fc794d67933415a882d7fb
declanbbbbb <declan.bnbg@gmail.com>
2021-11-15 11:23:18 -0600

    bruh

---
+++ template - Copy/sketch.js
@@ -0,0 +1,19 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(255);
+  rect(mouseX, mouseY, 100, 50);
+  
+}
+
+
